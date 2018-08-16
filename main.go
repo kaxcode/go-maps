b83c864d97907094289b72ee6043829069f9747d
Kenia <keniavalladarez@gmail.com>
2018-08-16 10:26:48 -0400

    Making a map (2nd way).

--- main.go
+++ main.go
@@ -5,10 +5,8 @@ import (
 )
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#4bf745",
-	}
+	colors := make(map[int]string)
 
+	colors[10] = "#ffffff"
 	fmt.Println(colors)
 }
